ea594e5516848dd6350100c9cfdd8cfd4fdfcdd3
ctsictai <ctsictai@gmail.com>
2021-06-17 21:47:34 +0900

    chore: lint-test

--- board/views.py
+++ board/views.py
@@ -1,11 +1,11 @@
-from drf_yasg.utils import no_body, swagger_auto_schema as schema
-from drf_yasg.openapi import IN_QUERY, TYPE_BOOLEAN, TYPE_STRING, Parameter
+from drf_yasg.openapi import IN_QUERY, TYPE_BOOLEAN, TYPE_STRING, Parameter, FORMAT_PASSWORD  # noqa
+from drf_yasg.utils import no_body  # noqa
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
-
-from rest_framework.decorators import action
+from rest_framework.decorators import action  # noqa
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, mixins
-from rest_framework.permissions import AllowAny
 
 from . import models, serializers
 
@@ -16,7 +16,7 @@ class BoardTest(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
     permission_classes = [AllowAny]
     serializer_class = serializers.BoardTestSerializer
 
-    @schema(
+    @swagger_auto_schema(
         operation_summary="board test",
         operation_description="""
         ### 세부내역
@@ -24,6 +24,7 @@ class BoardTest(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
         tags=["board"],
         manual_parameters=[
             Parameter("title", IN_QUERY, type=TYPE_STRING, description="제목"),
+            Parameter("test", IN_QUERY, type=TYPE_STRING, description="디스크립션_테스트", format=FORMAT_PASSWORD),
         ],
     )
     def list(self, request, *args, **kwargs):
@@ -38,11 +39,11 @@ class BoardTest(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    @schema(
+    @swagger_auto_schema(
         operation_summary="board-test-post  ",
-        request_body=serializers.BoardTestSerializer,
-        tags=["partner_notice"],
-        operation_description="test",
+        # request_body=serializers.BoardTestSerializer,
+        tags=["board_create"],
+        operation_description="post_test",
     )
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- cts_pro/urls.py
+++ cts_pro/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -48,7 +48,19 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += [
-        re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-        re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-        re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+        re_path(
+            r"^swagger(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^swagger/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^redoc/$",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
     ]
