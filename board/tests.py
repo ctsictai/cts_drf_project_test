163008a1bc7d3fb0e4ca62693c0bf5357478a866
ctsictai <ctsictai@gmail.com>
2021-12-16 23:41:05 +0900

    feature : pytest_added

--- board/tests.py
+++ board/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+import pytest
+
+from . import models
+
+
+@pytest.mark.django_db
+def test_create_board():
+    cre = models.Board.objects.create(
+        title="test",
+        sub_title="sub_test",
+        read_cnt=1,
+        writer="user",
+        created_at=datetime.now(),
+        context="",
+    )
+    assert cre
+
+
+@pytest.mark.django_db
+def test_search_board():
+    bo = models.Board.objects.all()
+    res = True if bo.count() == 0 else False
+    assert res == True
